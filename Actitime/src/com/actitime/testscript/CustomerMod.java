1bd71793c7e4f7f07e1bdb38f16293fb98e28c31
johnwb1996 <117428487+johnwb1996@users.noreply.github.com>
2023-04-15 22:18:18 +0530

    commit

--- Actitime/src/com/actitime/testscript/CustomerMod.java
+++ Actitime/src/com/actitime/testscript/CustomerMod.java
@@ -1,6 +1,5 @@
 package com.actitime.testscript;
 
-import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
@@ -9,24 +8,24 @@ import com.actitime.generic.BaseClass;
 @Listeners(com.actitime.generic.ListenerImplementation.class)
 public class CustomerMod extends BaseClass{
 		
-	//@Test
+	@Test
 	public static  void TestCreateCustomerdemo()
 	{
 		Reporter.log("TestCreateCustomer",true);
-		Assert.fail();
+		
 	}
 	
-
+	@Test
 	public static  void TestDeleteCustomerdemo()
 	{
 		Reporter.log("TestDeleteCustomer",true);
 	}
-
+	@Test
 	public static  void TestModifyCustomerdemo()
 	{
 		Reporter.log("TestModifyCustomer",true);
 
-		Assert.fail();
+		
 	}
 
 }
